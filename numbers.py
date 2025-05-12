6ad9e995cb1b0533677f98bdb1333f4eb678041e
Veronika Bucko <vbucko@dpd.sk>
2025-05-12 23:25:23 +0200

    Numbers

---
+++ numbers.py
@@ -0,0 +1,74 @@
+print(2+2)
+print(4-2)
+print(3*2)
+print(6/2)
+print(3.2 * 3.8)
+print(4//2)
+print(5//2)
+print(2 ** 2)
+
+#modulo/zvyšok
+print(4%2)
+print(5%2)
+
+veronika_age = 30
+monika_age = 28
+sum_ages = veronika_age + monika_age
+average = sum_ages/2
+print(sum_ages)
+print(average)
+print(sum_ages//2)
+
+age = 30
+negative_age = -30
+print(negative_age)
+
+score = 100
+score = score +1
+print(score)
+
+score += 1
+print(score)
+
+score -=1
+print(score)
+
+score *=2
+print(score)
+
+# Uloha c.1 Cvičenie: Vypočítaj priemer troch testovacích skóre
+
+# 1. Vytvor tri premenné na ukladanie testovacích skóre: score1, score2 a score3. Prirad im ľubovoľné číselné hodnoty.
+# 2. Vypočítaj súčet troch skóre a ulož ho do premennej s názvom total.
+# 3. Vydelením totalu číslom 3 vypočítaj priemer a ulož ho do premennej s názvom average.
+score1 = 100
+score2 = 200
+score3 = 300
+
+total = score1 + score2 + score3
+average = total/3
+print(total)
+print(average)
+
+# Uloha c.2 Cvičenie: Konverzia teploty
+
+# 1. Vytvor premennú s názvom celsius a prirad jej hodnotu teploty vo stupňoch Celzia.
+# 2. Vypočítaj ekvivalentnú teplotu vo stupňoch Fahrenheita a ulož ju do premennej s názvom fahrenheit. Pre konverziu môžeš použiť nasledujúci vzorec: fahrenheit = celsius * 9/5 + 32.
+celsius = 37.5
+fahrenheit = celsius * 9/5 + 32.
+print(celsius)
+print(fahrenheit)
+
+
+
+# Uloha c.3 Cvičenie: Obvod a obsah kruhu
+
+# 1. Vytvor premennú s názvom polomer a prirad jej hodnotu polomeru kruhu.
+# 2. Vypočítaj obvod kruhu a ulož ho do premennej s názvom obvod. Pre výpočet obvodu môžeš použiť vzorec: obvod = 2 * 3.14159 * polomer.
+# 3. Vypočítaj obsah kruhu a ulož ho do premennej s názvom obsah. Pre výpočet obsahu môžeš použiť vzorec: obsah = 3.14159 * polomer * polomer.
+
+polomer = 50
+obvod = 2 * 3.14159 * polomer
+print(obvod)
+obsah = 3.14159 * polomer * polomer
+print(obsah)
