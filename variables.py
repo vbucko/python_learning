6d5f0ae1d048e2d8bee6efe0e1ebf39045bd0791
Veronika Bucko <vbucko@dpd.sk>
2025-05-12 22:42:29 +0200

    Variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+age = 30
+print(age)
+
+name = "Veronika"
+print(name)
+
+# Uloha c.1
+# Zadefinuj premennu ktora bude obsahovat tvoj vek
+age = 30
+print(age)
+
+# Uloha c.2
+# Zadefinuj premennu ktora bude obsahovat tvoju vysku
+high = "174 cm"
+print(high)
+# Uloha pre sikovnych (je potrebne googlit)
+# Zadefinuj premennu, ktora bude obsahovat tvoje meno
+name = "Veronika"
+print(name)
