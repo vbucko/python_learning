16833682b7341426a4e3ae4273f86ee55bf47f6f
Veronika Bucko <vbucko@dpd.sk>
2025-05-12 23:49:39 +0200

    Variables_extesion

---
+++ variables_extension.py
@@ -0,0 +1,16 @@
+r = 10
+pi = 3.1415
+o =(1 + 2) * pi * r
+print(o)
+
+counter = 0
+print(counter)
+
+counter += 1
+print(counter)
+
+counter += 1 + 4
+print(counter)
+
+counter **=2
+print(counter)
