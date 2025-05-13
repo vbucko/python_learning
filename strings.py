46a3bbf90f2f7c0d1f5ef2f2e719354d3a850dc1
Veronika Bucko <vbucko@dpd.sk>
2025-05-13 23:08:09 +0200

    Strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+my_name = "Veronika Buc\nVeronika"
+
+print(my_name)
+print("Veronika")
+
+word = "python"
+print(word[5])
+print(word[0:2])
+print(word[0:3])
+print(word[1:3])
+print(word[-1:])
+
+
