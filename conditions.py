f104266ac5d4e6ec539b9d624c885811d571c680
Veronika Bucko <vbucko@dpd.sk>
2025-05-13 22:49:01 +0200

    Conditions

---
+++ conditions.py
@@ -0,0 +1,37 @@
+number = 2
+if number > 0:
+    print("Is positive")
+    print("Veronika")
+else:
+    print("Is negative")
+    print("Marek")
+
+print("This will be print everytime")
+
+if number > 0:
+    print("Is positive")
+    print("Veronika")
+else:
+    print("Is negative")
+    print("Marek")
+
+number = 2
+if number > 0:
+    print("Is positive")
+    print("Veronika")
+elif number < 0:
+    print("Is negative")
+else:
+    ("Is zero")
+
+number = 3
+if number % 2 ==0:
+    print("Is even")
+else:
+    print("is odd")
+
+number = 90
+if number >= 0 and number < 100:
+    print("Is in interval")
+else:
+    print("Is not in interval")
