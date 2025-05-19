70c7f063aab75d90c0bfb4d6111864b0538d3eb4
Veronika Bucko <vbucko@dpd.sk>
2025-05-19 21:30:34 +0200

    Strings2

---
+++ strings2.py
@@ -0,0 +1,42 @@
+single_quoted_string = 'This is string with "double qoutes" '
+print(single_quoted_string)
+
+print("String")
+print("String1", "String2")
+print(13)
+print(13, "String")
+
+multiline_string = """Hello, 
+how are you? """
+print(multiline_string)
+
+## String's operators
+string1 = "Veronika"
+string2 = "Buc"
+space = " "
+my_name = string1 + " " + string2
+my_name2 = string1 + space + string2
+print(my_name)
+print(my_name2)
+
+name="Veronika"
+multiple_name = name * 3
+print(multiple_name)
+
+name = "Veronika Buc"
+age = 30
+combine = name + str(age)
+print(combine)
+
+variable = "How long is my variable?"
+string_length = len(variable)
+string_length2 = len("Veronika" + "Buc")
+print(string_length)
+print(string_length2)
+
+name="Veronika"
+surname="Buc"
+space = " "
+full_name = name +space  + surname
+print(full_name)
+
